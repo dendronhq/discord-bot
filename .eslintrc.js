30018a8cf3a55e896719ef7b07ed83446278aa77
Mark Choi <hikchoi@gmail.com>
2022-07-21 23:31:29 +0900

    chore: update eslint and add nodem

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,31 @@ module.exports = {
     indent: ['error', 2],
     'no-multi-spaces': ['error'],
     'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'import/no-unresolved': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/export': 0,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', '@types'],
+      },
+      typescript: {},
+    },
   },
 };
